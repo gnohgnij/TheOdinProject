da41ed38d9ba2ee6efccf650d91f7720a540081d
Chong Jing Hong <congjinghong@gmail.com>
2020-12-23 14:28:39 +0800

    ...

--- Javascript Exercises/caesar/caesar.js
+++ Javascript Exercises/caesar/caesar.js
@@ -1,14 +1,14 @@
-const caesar = function() {
-    let toCode = 'Mjqqt, Btwqi!';
-    let shift = -5;
+const caesar = function(toCode, shift) {
     let output = '';
     for(let i=0; i<toCode.length; i++){
         if(toCode.charCodeAt(i) > 64 && toCode.charCodeAt(i) < 91 || toCode.charCodeAt(i) > 96 && toCode.charCodeAt(i) < 123){
             if(toCode.charAt(i) == toCode.charAt(i).toUpperCase()){
-                output += String.fromCharCode((toCode.charCodeAt(i) + shift - 65) % 26 + 65);
+                let mod = (toCode.charCodeAt(i) + shift - 65) % 26;
+                output += String.fromCharCode(mod + 65);
             }
             else if (toCode.charAt(i) == toCode.charAt(i).toLowerCase()){
-                output += String.fromCharCode((toCode.charCodeAt(i) + shift - 97) % 26 + 97);
+                let mod = (toCode.charCodeAt(i) + shift - 97) % 26;
+                output += String.fromCharCode(mod + 97);
             }
         }
         else
